51b3128d0f23d0e2d166b416bb8507d3dbaf044a
RodrigoJusto <rodrigojustoperez@gmail.com>
2025-06-11 04:39:36 +0200

    Ocultar contraseña y favicon y redimensionamiento de logo

--- relampagoweb/urls.py
+++ relampagoweb/urls.py
@@ -39,31 +39,4 @@ urlpatterns = [
 
     path("solicitar-reset-password/", solicitar_reset_password, name="solicitar-reset-password"),
     path("reset-password/<str:token>/", resetear_password, name="reset-password"),
-    
-    # # Recuperación de contraseña
-    # path(
-    #     'password_reset/',
-    #     CustomPasswordResetView.as_view(
-    #         template_name='contraseña/password_reset_form.html',
-    #         email_template_name='emails/password_reset.html',
-    #         subject_template_name='emails/password_reset_subject.txt',
-    #         success_url='/password_reset/done/'
-    #     ),
-    #     name='password_reset'
-    # ),
-    # path(
-    #     'password_reset/done/',
-    #     auth_views.PasswordResetDoneView.as_view(template_name='contraseña/password_reset_done.html'),
-    #     name='password_reset_done'
-    # ),
-    # path(
-    #     'reset/<uidb64>/<token>/',
-    #     auth_views.PasswordResetConfirmView.as_view(template_name='contraseña/password_reset_confirm.html'),
-    #     name='password_reset_confirm'
-    # ),
-    # path(
-    #     'reset/done/',
-    #     auth_views.PasswordResetCompleteView.as_view(template_name='contraseña/password_reset_complete.html'),
-    #     name='password_reset_complete'
-    # ),
 ]
