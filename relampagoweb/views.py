3b33aa362ba3ee60d1ab0cc8285c0b03d89537c4
Alvarciu <alvaroer2004@gmail.com>
2025-06-13 15:44:50 +0200

    Proyecto Final3

--- relampagoweb/views.py
+++ relampagoweb/views.py
@@ -85,7 +85,7 @@ def registro_view(request):
             return redirect('inicio')
     else:
         form = RegistroForm()
-    return render(request, 'registro.html', {'form': form})
+    return render(request, 'Registro.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
